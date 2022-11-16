67c5224f74524e8ed1c80fe894094d7851d8295b
Habilidebob de Jesus <80491104+habilidebob@users.noreply.github.com>
2022-11-16 18:05:35 -0300

    Layout do painel finalizado
    Layout do painel finalizado, sem implementações

--- admin/painel/index.php
+++ admin/painel/index.php
@@ -7,7 +7,7 @@ require('includes/cabecalho.php');
 <div class="container ">
     <div class="row mt-5">
         <div class="col">
-            <h2>Olá <?=$_SESSION['infos']['nome_completo'] ?>!</h2>
+            <h2>Olá <?= $_SESSION['infos']['nome_completo'] ?>!</h2>
         </div>
     </div>
     <div class="row justify-content-end">
@@ -25,35 +25,35 @@ require('includes/cabecalho.php');
     <div class="row mt-5">
         <div class="col-12">
             <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Foto</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Preco</th>
-                <th scope="col">Descrição</th>
-                <th scope="col">Categoria</th>
-                <th scope="col">Usuário</th>
-                <th scope="col">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td><img src="https://i.imgur.com/Otno8c7.png" width="50px"></td>
-                    <td>Nome do Produto</td>
-                    <td>R$ X.xx</td>
-                    <td>Aqui encontramos as informações do produto.</td>
-                    <td>Alimentação</td>
-                    <td>Jorjão Pimpão</td>
-                    <td>
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-danger btn-sm" type="button"><i class="bi bi-x-circle-fill"></i></button>
-                            <button class="btn btn-primary btn-sm" type="button"><i class="bi bi-pencil-square"></i></button>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Preco</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col">Categoria</th>
+                        <th scope="col">Usuário</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td><img src="https://i.imgur.com/Otno8c7.png" width="50px"></td>
+                        <td>Nome do Produto</td>
+                        <td>R$ X.xx</td>
+                        <td>Aqui encontramos as informações do produto.</td>
+                        <td>Alimentação</td>
+                        <td>Jorjão Pimpão</td>
+                        <td>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-danger btn-sm" type="button"><i class="bi bi-x-circle-fill"></i></button>
+                                <button class="btn btn-primary btn-sm" type="button"><i class="bi bi-pencil-square"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
@@ -64,42 +64,81 @@ require('includes/cabecalho.php');
 
 <!-- Modal Produto -->
 <div class="modal fade" id="modalCadastro" tabindex="-1" aria-labelledby="modalCadastro" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modalCadastro">Cadastrar Produto</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Cadastrar</button>
-      </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="modalCadastro">Cadastrar Produto</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="nomeProduto" class="form-label">Nome: </label>
+                        <input type="text" class="form-control" id="nomeProduto" name="nomeProduto" aria-describedby="produtoHelp">
+                        <div id="produtoHelp" class="form-text">Nome do produto que aparecerá no sistema.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="precoProduto" class="form-label">Preço: </label>
+                        <input type="number" step=".01" class="form-control" id="precoProduto" name="precoProduto" aria-describedby="precoHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="descricaoProduto" class="form-label">Descricao: </label>
+                        <textarea class="form-control" id="descricaoProduto" name="descricaoProduto" aria-describedby="descricaoHelp"></textarea>
+                        <div id="descricaoHelp" class="form-text">A descrição do produto que será exibida logo após a imagem na página inicial.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="fotoProduto" class="form-label">Foto: </label>
+                        <input type="file" class="form-control" id="fotoProduto" name="fotoProduto" aria-describedby="fotoHelp"></textarea>
+                        <div id="fotoHelp" class="form-text">Arquivo em jpg ou png que representará o produto no sistema.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="categoriaProduto" class="form-label" >Categoria: </label>
+                        <select class="form-select" aria-label="Default select example" id="categoriaProduto" name="categoriaProduto">
+                            <option selected>Escolha a categoria</option>
+                            <!-- Os campos abaixo deverão ser populados automaticamente com PHP: -->
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <!-- O ID do usuário deve ser obtido automaticamente pelo controle de sessão! -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+                </form>
+            </div>
+
+        </div>
     </div>
-  </div>
 </div>
 
 
 
 <!-- Modal Categoria-->
 <div class="modal fade" id="modalCategoria" tabindex="-1" aria-labelledby="modalCategoria" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modalCategoria">Cadastrar Categoria</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Cadastrar</button>
-      </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="modalCategoria">Cadastrar Categoria</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="">
+                    <div class="mb-3">
+                        <label for="nomeCategoria" class="form-label">Nome da Categoria:</label>
+                        <input type="text" class="form-control" name="nomeCategoria" id="nomeCategoria" aria-describedby="categoriaHelp">
+                        <div id="categoriaHelp" class="form-text">A categoria deve ser única e não deve existir no sistema.</div>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            </form>
+        </div>
     </div>
-  </div>
 </div>
 
 <?php
--- admin/painel/perfil.php
+++ admin/painel/perfil.php
@@ -5,7 +5,41 @@ require('includes/cabecalho.php');
 ?>
 
 
-<h1>Seu perfil</h1>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-4">
+            <img class="rounded" src="static/perfil.png">
+        </div>
+        <div class="col-8">
+            <h1 class="display-3">Seu Perfil</h1>
+            <form action="" method="">
+                <div class="mb-3">
+                    <label for="nomeUsuario" class="form-label">Nome:</label>
+                    <input type="text" value="<?= $_SESSION['infos']['nome_completo'] ?>" class="form-control" name="nomeUsuario" id="nomeUsuario">
+                </div>
+                <div class="mb-3">
+                    <label for="emailUsuario" class="form-label">Email:</label>
+                    <input type="text" value="<?= $_SESSION['infos']['email'] ?>" class="form-control" name="emailUsuario" id="emailUsuario">
+                </div>
+                <div class="mb-3">
+                    <label for="senhaUsuario" class="form-label">Senha atual:</label>
+                    <input type="password" class="form-control" name="senhaUsuario" id="senhaUsuario">
+                </div>
+                <div class="mb-3">
+                    <label for="novaSenhaUsuario" class="form-label">Nova senha:</label>
+                    <input type="password" class="form-control" name="novaSenhaUsuario" id="novaSenhaUsuario">
+                </div>
+                <div class="mb-3">
+                    <label for="novaSenhaUsuario1" class="form-label">Reptir nova senha:</label>
+                    <input type="password" class="form-control" name="novaSenhaUsuario1" id="novaSenhaUsuario1">
+                </div>
+                <div class="d-grid gap-2">
+                    <button class="btn btn-success" type="submit">Alterar Informações</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php
 
