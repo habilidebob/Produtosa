af7fc20a80621861931b3b03ec6032d66d289a34
Habilidebob de Jesus <80491104+habilidebob@users.noreply.github.com>
2022-11-16 13:24:42 -0300

    Update cabecalho.php

--- admin/painel/includes/cabecalho.php
+++ admin/painel/includes/cabecalho.php
@@ -12,11 +12,11 @@
     <!-- Barra de navegação -->
     <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Painel de Gerencialmento</a>
+    <a class="navbar-brand fw-bolder" href="#">Painel de Gerencialmento</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse d-flex justify-content-start" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="index.php">Início</a>
